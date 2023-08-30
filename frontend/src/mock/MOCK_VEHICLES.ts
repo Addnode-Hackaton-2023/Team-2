4a37e48ab53f39abe09f1e8df7d4541f19f287e1
Per Fröjd <per.frojd@decerno.se>
2023-08-30 19:40:28 +0200

    Merge things

---
+++ frontend/src/mock/MOCK_VEHICLES.ts
@@ -0,0 +1,12 @@
+export function mockVehicles(): Array<Vehicle> {
+  return [
+    {
+      MaxCargo: '200',
+      VehicleId: '1',
+    },
+    {
+      MaxCargo: '250',
+      VehicleId: '2',
+    },
+  ];
+}
