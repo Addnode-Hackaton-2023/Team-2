eaca18a607cc1d2922a03460926069dde59fda5f
Per Fröjd <per.frojd@decerno.se>
2023-08-30 19:32:54 +0200

    Some sort of calendar (and other things)

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -4,15 +4,18 @@ import Link from 'next/link';
 
 export function Sidebar() {
   return (
-    <div className='flex h-screen flex-col justify-between border-e bg-white min-w-fit'>
+    <div className='flex h-full flex-col justify-between bg-white min-w-fit'>
       <div className='px-4 py-6'>
         <SidebarLogo />
         <ul className='mt-6 space-y-1'>
           <SidebarRow text='Dashboard' url={'/'} />
           <SidebarRow text='Förare' url={'/driver'} />
+          <SidebarRow indent text='Boka förare' url={'/calendar'} />
           <SidebarRow text='Karta' url={'/map'} />
           <SidebarRow text='Mottagare' url={'/recipient'} />
+          <SidebarRow indent text='Ny Mottagare' url={'/recipient/add'} />
           <SidebarRow text='Affär' url={'/store'} />
+          <SidebarRow text='Fordon' url={'/vehicle'} />
         </ul>
       </div>
       <SidebarUser avatar='' email='user@example.com' name='Example User' />
@@ -23,14 +26,17 @@ export function Sidebar() {
 interface SidebarRow {
   text: string;
   url: string;
+  indent?: boolean;
 }
 
-function SidebarRow({ text, url }: SidebarRow) {
+function SidebarRow({ text, url, indent }: SidebarRow) {
   return (
     <li>
       <Link
         href={url}
-        className='block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700'
+        className={`block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 ${
+          indent ? 'pl-4' : 'pl-0'
+        }`}
       >
         {text}
       </Link>
--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -4,27 +4,14 @@ import { Sidebar } from '@/components/sidebar';
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
   return (
-    <div className='flex'>
+    <div className='flex h-screen'>
       <Sidebar />
-      <main className='w-screen bg-gray-200 text-black'>{children}</main>
+      <main className='w-screen overflow-y-auto bg-gray-200 text-black'>
+        {children}
+      </main>
     </div>
   );
 }
 const Navbar = () => {
-  return (
-    <Sidebar />
-    // <nav className='flex justify-center items-center h-10 gap-4'>
-    //   {PAGES.map(({ title, path }) => (
-    //     <div
-    //       key={path}
-    //       className={`px-2 py-1 cursor-pointer ${
-    //         pathname === path ? 'border-b border-gray-100' : ''
-    //       } hover:bg-gray-500`}
-    //       onClick={() => onClick(path)}
-    //     >
-    //       {title}
-    //     </div>
-    //   ))}
-    // </nav>
-  );
+  return <Sidebar />;
 };
