346cebf14ef6702834d32bc5e300ad4ec3357c4a
Per Fröjd <per.frojd@decerno.se>
2023-08-30 13:27:01 +0200

    Base packages and some styling

--- frontend/src/pages/route/index.tsx
+++ frontend/src/pages/route/index.tsx
@@ -1,3 +1,18 @@
-export function RoutePage() {
-  return <div>Hello World</div>;
+import Layout from '@/layout/layout';
+import { ReactElement, useEffect, useRef } from 'react';
+import Map from '../../components/map';
+
+import { setDefaultOptions } from 'esri-loader';
+setDefaultOptions({ css: true });
+
+export default function RoutePage() {
+  return (
+    <div>
+      <h1>Route Page</h1>
+    </div>
+  );
 }
+
+RoutePage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
