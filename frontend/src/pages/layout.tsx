bc9d5790b1f6b0bfdfd520ad663f216a1520d0fe
Per Fröjd <per.frojd@decerno.se>
2023-08-30 16:04:34 +0200

    Added a sidebar to all pages with links

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,12 +1,13 @@
 import { PropsWithChildren, useCallback } from 'react';
 import { useRouter } from 'next/router';
+import { Sidebar } from '@/components/sidebar';
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
   return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-    </>
+    <div className='flex'>
+      <Sidebar />
+      <main className='w-screen'>{children}</main>
+    </div>
   );
 }
 
@@ -18,22 +19,20 @@ const PAGES = [
 ];
 
 const Navbar = () => {
-  const { pathname, push } = useRouter();
-  const onClick = useCallback((newPath: string) => push(newPath), [push]);
-
   return (
-    <nav className='flex justify-center items-center h-10 gap-4'>
-      {PAGES.map(({ title, path }) => (
-        <div
-          key={path}
-          className={`px-2 py-1 cursor-pointer ${
-            pathname === path ? 'border-b border-gray-100' : ''
-          } hover:bg-gray-500`}
-          onClick={() => onClick(path)}
-        >
-          {title}
-        </div>
-      ))}
-    </nav>
+    <Sidebar />
+    // <nav className='flex justify-center items-center h-10 gap-4'>
+    //   {PAGES.map(({ title, path }) => (
+    //     <div
+    //       key={path}
+    //       className={`px-2 py-1 cursor-pointer ${
+    //         pathname === path ? 'border-b border-gray-100' : ''
+    //       } hover:bg-gray-500`}
+    //       onClick={() => onClick(path)}
+    //     >
+    //       {title}
+    //     </div>
+    //   ))}
+    // </nav>
   );
 };
