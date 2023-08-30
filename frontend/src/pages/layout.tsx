6f71df0576116fa491ab9e23e4485852db50d408
Viktor Jonson <112390018+viktorjonson-decerno@users.noreply.github.com>
2023-08-30 17:24:53 +0200

    Cleanup & add tab highlight

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,5 +1,4 @@
-import { PropsWithChildren, useCallback } from 'react';
-import { useRouter } from 'next/router';
+import { PropsWithChildren } from 'react';
 import { Sidebar } from '@/components/sidebar';
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
@@ -10,21 +9,3 @@ export default function Layout({ children }: PropsWithChildren<unknown>) {
     </div>
   );
 }
-const Navbar = () => {
-  return (
-    <Sidebar />
-    // <nav className='flex justify-center items-center h-10 gap-4'>
-    //   {PAGES.map(({ title, path }) => (
-    //     <div
-    //       key={path}
-    //       className={`px-2 py-1 cursor-pointer ${
-    //         pathname === path ? 'border-b border-gray-100' : ''
-    //       } hover:bg-gray-500`}
-    //       onClick={() => onClick(path)}
-    //     >
-    //       {title}
-    //     </div>
-    //   ))}
-    // </nav>
-  );
-};
